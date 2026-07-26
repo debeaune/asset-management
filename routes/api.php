6a983171533b61309bdf33855fcc3023bddefbb3
Marie Laure Debeaune <ml.debeaune@gmail.com>
2026-07-26 16:02:08 +0200

    Add Sanctum authentication

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,17 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\LocationController;
 use App\Http\Controllers\EquipmentController;
 use App\Http\Controllers\TicketController;
+use App\Http\Controllers\AuthController;
 
-Route::apiResource('categories', CategoryController::class);
-Route::apiResource('locations', LocationController::class);
-Route::apiResource('equipments', EquipmentController::class);
-Route::apiResource('tickets', TicketController::class);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('locations', LocationController::class);
+    Route::apiResource('equipments', EquipmentController::class);
+    Route::apiResource('tickets', TicketController::class);
+});
